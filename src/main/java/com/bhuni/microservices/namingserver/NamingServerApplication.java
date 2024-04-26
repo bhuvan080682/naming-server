24ceb4c2bbd152de0f79d1798a4419be92bb34ee
bhuvan080682 <bhuvan080682@gmail.com>
2024-04-26 21:13:40 +0530

    modified code

--- src/main/java/com/bhuni/microservices/namingserver/NamingServerApplication.java
+++ src/main/java/com/bhuni/microservices/namingserver/NamingServerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("You are in Naming Server");
 		SpringApplication.run(NamingServerApplication.class, args);
 	}
 
